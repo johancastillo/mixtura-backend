ad7b712322a370ec280fbc15a87180a9d5d4c38e
Johan Castillo <jcjohan2707@gmail.com>
2020-11-19 09:10:58 -0400

    Pages on the server

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,9 +1,23 @@
+# Import micro Framework Flask
 from flask import Flask
 
+# Intance of Flask class
 app = Flask(__name__)
 
+# Home page
 @app.route('/')
 def index():
-    return 'Hola mundo'
+    return 'Hello world'
 
+# About page
+@app.route('/about')
+def about():
+    return 'It is page of about'
+
+# Services page
+@app.route('/services')
+def services():
+    return 'It is page of services'
+
+# Run application
 app.run()
